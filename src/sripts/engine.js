8dc963afe7e18341f16e53dc48434c85c8bbc7e5
https://github.com/vcr1985 <ramosvando@gmail.com>
2024-01-14 17:23:23 -0300

    game-memory

---
+++ src/sripts/engine.js
@@ -0,0 +1,57 @@
+const emojis = [
+  "😜",
+  "😜",
+  "🧐",
+  "🧐",
+  "🤩",
+  "🤩",
+  "😤",
+  "😤",
+  "😍",
+  "😍",
+  "🤡",
+  "🤡",
+  "🤖",
+  "🤖",
+  "👹",
+  "👹",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  box.onclick = handlClik;
+  document.querySelector(".game").appendChild(box);
+}
+
+function handlClik() {
+  if (openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
+  if (openCards.length === 2) {
+    setTimeout(checkMatch, 500);
+
+    console.log(openCards);
+  }
+
+  function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+      openCards[0].classList.add("boxMatch");
+      openCards[1].classList.add("boxMatch");
+    } else {
+      openCards[0].classList.remove("boxOpen");
+      openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+      alert("Você venceu !");
+    }
+  }
+}
